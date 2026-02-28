730cedc1f573cc3068f93b003ae537bef5f5022b
unknown <raikwarshivam635@gmail.com>
2026-02-28 18:07:35 +0530

    fixed nextauth v4 setup

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,39 @@
-export { GET, POST } from "@/app/_lib/auth";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { createGuest, getGuest } from "@/app/_lib/data-service";
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ user }) {
+      const existingGuest = await getGuest(user.email);
+
+      if (!existingGuest) {
+        await createGuest({
+          email: user.email,
+          fullName: user.name,
+        });
+      }
+
+      return true;
+    },
+
+    async session({ session }) {
+      const guest = await getGuest(session.user.email);
+      if (guest) session.user.guestId = guest.id;
+      return session;
+    },
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
